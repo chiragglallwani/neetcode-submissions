ae1f0cb9a6701ed624e0506f9b307e59f3862308
Chirag Lalwani <chiragglallwani@gmail.com>
2026-06-21 22:16:11 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+        min_sqrt = float("inf")
+        l, r = 0, x
+        while l <= r:
+            mid = (l + r) // 2
+            squareof_mid = mid * mid
+            print(squareof_mid)
+            if squareof_mid == x:
+                return mid
+            elif squareof_mid < x:
+                l = mid + 1
+                min_sqrt = mid
+            else:
+                r = mid - 1
+           
+        return int(min_sqrt)
